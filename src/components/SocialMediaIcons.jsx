45181115ea804c50921da82d3f429d7504da7ae3
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-06-27 14:21:08 -0300

    Landing finished

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/in/pablo-maldonado-528972144/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/linkedin.png" alt="linkedin-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://twitter.com/Pablo54903404"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/twitter.png" alt="twitter-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com/pablo.maldonado.9"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/facebook.png" alt="facebook-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com/paaablo2086/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/instagram.png" alt="instagram-link" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
